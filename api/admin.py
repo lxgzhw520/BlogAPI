43549466e7e34af3151c4ccd2cb29594aef6e931
lxgzhw520 <1156956636@qq.com>
2019-07-17 10:18:40 +0800

    数据库模型创建并迁移完成

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Article, Category, Comment, Good
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Good)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 """
 博客功能分析:
@@ -31,3 +32,106 @@ from django.db import models
 4.分类
     分类名称
 """
+
+"""
+1.文章表
+    文章标题
+    发布时间
+    阅读人数
+    阅读数量
+    点赞数量
+    点踩数量
+    评论数量
+    文章内容
+"""
+
+
+class User(models.Model):
+    name = models.CharField(max_length=24, verbose_name="用户名")
+    password = models.CharField(max_length=124, verbose_name="密码")
+    avatar = models.ImageField(upload_to='avatar', verbose_name="头像")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = '用户表'
+
+
+class Article(models.Model):
+    author = models.ForeignKey(verbose_name="作者", to="User", on_delete=models.CASCADE)
+    name = models.CharField(max_length=24, verbose_name="标题",unique=True)
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name="发布时间")
+    views = models.IntegerField(verbose_name="浏览量")
+    good_num = models.IntegerField(verbose_name="点赞数量")
+    bad_num = models.IntegerField(verbose_name="点踩数量")
+    comment_num = models.IntegerField(verbose_name="评论数量")
+    content = RichTextUploadingField(verbose_name="内容", config_name='ck')
+    category = models.ForeignKey(to='Category', on_delete=models.CASCADE, verbose_name="文章分类")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = '文章表'
+
+
+"""
+2.评论表
+    评论时间
+    评论人
+    评论内容
+    评论文章
+"""
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=24, verbose_name="评论标题")
+    article = models.ForeignKey(to='Article', on_delete=models.CASCADE, verbose_name='评论文章')
+    user = models.ForeignKey(to='User', verbose_name="评论用户", on_delete=models.CASCADE)
+    pu_date = models.DateTimeField(auto_now_add=True, verbose_name="评论时间")
+    content = RichTextUploadingField(verbose_name="评论内容", config_name='ck')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = '评论表'
+
+
+"""
+3.点赞表
+    点赞文章
+    点赞用户
+    点赞数量
+    点踩数量
+"""
+
+
+class Good(models.Model):
+    article = models.ForeignKey(to='Article', verbose_name="点赞文章", on_delete=models.CASCADE)
+    user = models.ForeignKey(to="User", verbose_name="点赞用户", on_delete=models.CASCADE)
+    good_num = models.IntegerField(verbose_name="点赞数量", default=0)
+    bad_num = models.IntegerField(verbose_name="点踩数量", default=0)
+
+    def __str__(self):
+        return self.user.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = '点赞表'
+
+
+"""
+4.分类
+    分类名称
+"""
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=24, verbose_name="分类名称")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = '分类表'
